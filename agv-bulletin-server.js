ed2360efaa31d6271674cebd39b5092857dce99f
beenice19 <byron217@yahoo.com>
2026-05-17 22:07:14 -0500

    Make bulletin server Render-ready

--- agv-bulletin-server.js
+++ agv-bulletin-server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
 
-const PORT = 8785;
+const PORT = Number(process.env.PORT || 8785);
 const DATA_FILE = path.join(__dirname, "agv-bulletins.json");
 
 const app = express();
